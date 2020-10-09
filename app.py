71d584241e4db48ad1d2c204525deda995066434
ArunKara <64599986+ArunKara@users.noreply.github.com>
2020-10-09 14:55:04 -0400

    make it better

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
 from flask import Flask, render_template
 import pandas as pd
 from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
 from config import cxnstring
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 engine = create_engine(cxnstring, pool_recycle=3600)
+con=engine.connect()
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-app.route("/sqltest")
-def psqltest():
-    response = pd.read_sql("SELECT * FROM actors LIMIT 10", engine)
-    return Response(response.to_json(orient="records",date_format="iso"), mimetype="application/json")
-
-#endpoints! , if you have multiple pages or
-#if you're reading from database that will
-# run python function from database
-#@app.route("/endpoint")
-
+@app.route("/investors2")
+def investors2():
+    response = pd.read_sql("SELECT * FROM investors2", con=engine)
+    response2_dict = response.to_dict(orient = "records")
+    return jsonify(response2_dict)
+    
+@app.route("/investors_copy")
+def investors_copy():
+    response2 = pd.read_sql("SELECT * FROM investors_copy", con=engine)
+    response3_dict = response2.to_dict(orient = "records")
+    return jsonify(response3_dict)
 
 if __name__ == "__main__":
     app.run()
--- init_db.py
+++ init_db.py
@@ -2,8 +2,5 @@ import pandas as pd
 from sqlalchemy import create_engine
 from config import cxnstring
 
-## i have to change "actors_csv" and "actors" as well as "my_db.sqlite"
-pd.read_csv("actors_csv").to_sql(name="actors", con=create_engine(cxnstring)) #create_engine("sqlite:///my_db.sqlite"))
-
-## having trouble running init_db, says actors_csv doesn't exist,
-## i know i need to change it but idk what to
+pd.read_csv("investors_copy.csv").to_sql(name="investors_copy", con=create_engine(cxnstring))
+pd.read_csv("investors2.csv").to_sql(name="investors2", con=create_engine(cxnstring))
