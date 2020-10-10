e6afb51842472775038705cae9fa2c4855ae7f92
beccae1225@gmail.com <beccae1225@gmail.com>
2020-10-10 12:05:12 -0400

    hope this works

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -3,8 +3,8 @@ console.log("buildPlot");
 function buildPlot(data) {
 
     //read in data
-    d3.json("results.json").then((data) => {
-        //console.log(data);
+    d3.json("../Json Files/investors.json").then((data) => {
+        
 
         let investor = data.map(d => d.name);
             //console.log(`Investors: ${investor}`);
@@ -12,32 +12,18 @@ function buildPlot(data) {
         let portfolioSize = data.map(d => d.portfolio_size)
                 //console.log(`Portfolio Size: ${portfolioSize}`);
    
-        //let industry = data.map(d => d.industry);
+        let industry = data.map(d => d.industry);
             //console.log(`Industry: ${industry}`); 
             
-        //let location = data.map(d => d.location);
-            //console.log(`Location: ${location}`);
-            
-        let investorType = data.map(d => d.type);
+        let investorType = data.map(d => d.type_of_investor);
             console.log(`Type: ${investorType}`);
 
-        //let wealth = data.map(d => d.wealth_worth_in_billions);
+        let wealth = data.map(d => d.wealth_worth_in_billions);
 
-        let avgGrowth = data.map(d => d.avg_growth_score);
-            //console.log(`Avg Growth Score: ${avgGrowth}`);
-
-        let dealsPC = data.map(d => d.deals_per_company);
-            //console.log(`Deals Per Company: ${dealsPC}`);
 
         let numberDeals = data.map(d => d.number_of_deals);
         //let topNumberDeals = numberDeals.slice(0,10).reverse();
             //console.log(`# of Deals: ${numberDeals}`);
-
-        let offeredFunds = data.map(d => d.three_year_funds_offered);
-            //console.log(`Funds offered: ${offeredFunds}`);
-
-        let soldFunds = data.map(d => d.three_year_funds_sold);
-            //console.log(`Funds Sold: ${soldFunds}`);
 
         let website = data.map(d => d.website);
             //console.log(`Website: ${website}`);
@@ -49,18 +35,18 @@ function buildPlot(data) {
         let trace1 = {
             x:investor,
             y: numberDeals,
+            fill: {color: 'rgb(185, 163, 192)'},
             name: "Number of Deals",
             type: 'scatter',
-            marker: {color: 'rgb(82, 1, 110)'}
+            marker: {color: 'rgb(185, 163, 192)'}
             };
 
             let trace2= {
                 x: investor,
-                y: portfolioSize,
-                text: investor,
-                name: "Portfolio Size",
+                y: wealth,
+                name: "Wealth (in $Billions)",
                 type: "bar", 
-                marker: {color: 'rgb(185, 163, 192)'},
+                marker: {color: 'rgb(82, 1, 110)'},
             };
 
             let barData = [trace1, trace2];
@@ -69,9 +55,12 @@ function buildPlot(data) {
                 autosize: false,
                 width: 700,
                 height: 700,
-                showlegend: false,
+                showlegend: true,
+                xaxis: {
+                    showticklabels: false
+                },
                 title: {
-                    text: `Investor Portfolio Comparison`,
+                    text: `Portfolio Size & Investor Wealth `,
                     font: {
                         family: 'Segoe UI Light',
                         size: 34
@@ -92,6 +81,7 @@ function buildPlot(data) {
         mode: "markers", 
         marker: {
             size: portfolioSize,
+            sizemode: 'area',
             color: numberDeals
         },
         text: investor
@@ -104,14 +94,14 @@ function buildPlot(data) {
         width: 700,
         height: 750,
         title: {
-            text: "Investor Wealth and Portfolio Comparison",
+            text: "Number of Deals by Portfolio Size ",
             font: {
                 family: 'Segoe UI Light',
                 size: 34
             },
             
         },
-        xaxis: {title: "Wealth ($billions)"},
+        xaxis: {title: "Number of Deals"},
         yaxis: {title: "Portfolio Size"}
     };
 
@@ -145,24 +135,43 @@ console.log("GetData");
 
 function getData(data) {
     
-    d3.json("results.json").then((data) => {
-
-        let filteredData = data;
+    d3.json("../Json Files/investors.json").then((data) => {
 
         let types = d3.select("#selDataset").property("value");
         
+        let newBarData = [];
+        let t1 = [];
+        let t2 = [];
+        let t3 = [];
 
-        if (types) {
-            filteredData = filteredData.filter(row => row.type ===types);
+        //let newData = data;
+        let filteredData = data.filter(row => row.type_of_investor ===types)
 
+        //for (let i = 0; i < filteredData.length; i++) {
+
+        //if (types === filteredData.type_of_investor) {
+            
+            //types = filteredData.type
+
+            //for (let i = 0; i < filteredData.length; i++)
+            
+            t1.push(filteredData.name);
+            t2.push(filteredData.wealth);
+            t3.push(filteredData.number_of_deals);
+            
+            newBarData.push(t1, t2, t3);
+
+            let update1 = {'x': newBarData[0], 'y': newBarData[1,2]};
             buildTable(filteredData);
-            updatePlotly(filteredData);
-        }
+            Plotly.restyle("multi-bar", update1, newBarData[0,1,2]);
+            //updatePlotly(filteredData);
+        //} };
+        console.log(filteredData);console.log(t1);
     });   
 };
 
 function optionChanged(filteredData) {
-    updatePlotly(filteredData);
+    //updatePlotly(filteredData);
     buildTable(filteredData);
     getData(filteredData);
     
@@ -180,7 +189,7 @@ function init() {
    //let selType = data.filter(data => data.type === sel);
         //console.log(selType);
 
-        d3.json("results.json").then((data) => {
+        d3.json("../Json Files/results.json").then((data) => {
         
             buildPlot(data);
             buildTable(data);
@@ -192,11 +201,38 @@ function init() {
 
 };
 
-console.log("updatePlotly")
 
-function updatePlotly(filteredData) {
-    Plotly.restyle("bubble", filteredData);
-    Plotly.restyle("multi-bar", filteredData);
-  }
+//function updatePlotly(filteredData) {
+
+    //d3.json("results.json").then((data) => {
+
+        //let types = d3.select("#selDataset").property("value");
+        
+        //let newBubbleData = [];
+
+        //let bubbleX = Object.value(data.number_of_deals);
+        //let bubbleY = Object.value(data.portfolio_size);
+        
+        //let x1 = newBarData.name;
+
+        //if (types === filteredData.type) {
+            //let filteredData = filteredData.filter(row => row.type ===types);
+            
+            //let bubbleX = Object.value(filteredData.number_of_deals);
+            //let bubbleY = Object.value(filteredData.portfolio_size);;
+            
+           // newBarData.concat(bubbleX);
+         //   newBarData.concat(bubbleY);
+            //newBarData.push(x1)
+
+       //     let update1 = {'x': newBarData[0], 'y': newBarData[1]};
+            //buildTable(filteredData);
+            //updatePlotly(filteredData);
+     //   } console.log(newBubbleData)
+   // });   
+    
+   // Plotly.restyle("bubble", "update1", [0,1]);
+    //Plotly.restyle("multi-bar", filteredData);
+ // };
 
 init();
